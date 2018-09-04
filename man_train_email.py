f65fbcdce8bdeb47780861293ca1c0eaecc2c4e6
rgregory <Rusti@SWA2287.swanton.com>
2018-09-04 09:27:34 -0400

    changed year

--- man_train_email.py
+++ man_train_email.py
@@ -12,7 +12,7 @@ scope = ['https://spreadsheets.google.com/feeds']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
-sheet = client.open('Mandatory Training Responses 17-18').sheet1
+sheet = client.open('Mandatory Training Responses 18-19').sheet1
 
 staff_responses = sheet.get_all_records()
 
